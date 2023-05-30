e5d4fb29c5776ab8af74ecb34dd956f4f52480d0
Anton Lukyanov <alukywork@gmail.com>
2023-05-30 13:22:54 +0200

    Initial commit

---
+++ LC_705/DesignHashSet.cs
@@ -0,0 +1,45 @@
+namespace LeetCode.LC_705;
+
+// https://leetcode.com/problems/design-hashset/
+
+public class MyHashSet
+{
+
+    public MyHashSet()
+    {
+
+    }
+
+    public void Add(int key)
+    {
+
+    }
+
+    public void Remove(int key)
+    {
+
+    }
+
+    public bool Contains(int key)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class DesignHashSet
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        var key = 5;
+        MyHashSet obj = new MyHashSet();
+        obj.Add(key);
+        obj.Remove(key);
+        bool param_3 = obj.Contains(key);
+    }
+}
