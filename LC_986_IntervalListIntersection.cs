e698d6ad4f8890700e0ace0c6826a52b60fa2982
Anton Lukyanov <alukywork@gmail.com>
2023-12-25 17:43:24 +0300

    LC_986 interval list intersection. Yandex tech interview 1, task 2

---
+++ LC_986_IntervalListIntersection.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode;
+
+public class Solution
+{
+    public int[][] IntervalIntersection(int[][] firstList, int[][] secondList)
+    {
+        var result = new List<int[]>();
+
+        var index1 = 0;
+        var index2 = 0;
+
+        while (index1 < firstList.Length && index2 < secondList.Length)
+        {
+            var interval1 = firstList[index1];
+            var interval2 = secondList[index2];
+
+            var resultingPair = new int[2];
+            resultingPair[0] = Math.Max(interval1[0], interval2[0]);
+
+            if (interval1[1] > interval2[1])
+            {
+                resultingPair[1] = interval2[1];
+                index2++;
+            }
+            else
+            {
+                resultingPair[1] = interval1[1];
+                index1++;
+            }
+
+            if (resultingPair[0] <= resultingPair[1])
+            {
+                result.Add(resultingPair);
+            }
+        }
+
+        return result.ToArray();
+    }
+}
