ea618df314641e0ca27310f66812d9594b571ac3
Anton Lukyanov <alukywork@gmail.com>
2024-01-09 03:01:48 +0300

    LC 322: Beats 100% runtime, average memory

--- LC_322/CoinChange.cs
+++ LC_322/CoinChange.cs
@@ -4,6 +4,47 @@ public class Solution
 {
     public int CoinChange(int[] coins, int amount)
     {
+        if (amount == 0)
+        {
+            return 0;
+        }
+
+        var weights = new short[amount + 1];
+
+        for (var i = coins.Length - 1; i >= 0; i--)
+        {
+            int coin = coins[i];
+            for (short currentValue = 0; currentValue <= amount; currentValue++)
+            {
+                if (weights[currentValue] == 0 && currentValue != 0)
+                {
+                    continue;
+                }
+
+                var nextValue = currentValue + coin;
+                short nextWeight = (short)(weights[currentValue] + 1);
+
+                if (nextValue > amount)
+                {
+                    break;
+                }
+
+                if (weights[nextValue] == 0 || weights[nextValue] > nextWeight)
+                {
+                    weights[nextValue] = (short)nextWeight;
+                }
+            }
+        }
+
+        return weights[amount] == 0 ? -1 : weights[amount];
+    }
+}
+
+
+public class SolutionV2
+{
+    public int CoinChange(int[] coins, int amount)
+    {
         Array.Sort(coins);
 
         var weights = new int[amount + 1];
